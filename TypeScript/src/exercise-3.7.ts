90bb9f3fdf251637fa2ee66e6390a5abc7dc1bda
NicolasFontana <nico.fontana12@gmail.com>
2023-05-08 14:07:16 -0300

    Exercise 3.7 finished

---
+++ TypeScript/src/exercise-3.7.ts
@@ -0,0 +1,20 @@
+const numberCollection: number[] = [];
+const stringCollection: string[] = [];
+
+function pushToCollection<Type>(item: Type, collection: Type[]): Type[] {
+    collection.push(item);
+    return collection;
+}
+
+// Anadir algunas cosas a las colecciones
+pushToCollection(false, stringCollection);
+pushToCollection("hi", stringCollection);
+pushToCollection([], stringCollection);
+
+pushToCollection(1, numberCollection);
+pushToCollection(2, numberCollection);
+pushToCollection(3, numberCollection);
+
+const incrementedByTwo = numberCollection.map(num => num + 2);
+
+console.log("[Ejercicio 3.7]", `[${incrementedByTwo}] debe ser igual a [3,4,5]`);

b1b9939317abc53f8f89fffca94145201c90a003
NicolasFontana <nico.fontana12@gmail.com>
2023-06-18 22:49:53 -0300

    Added navigation with react router dom

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,35 +1,42 @@
-import Props from "./Components/Props";
-import State from "./Components/State";
-import Events from "./Components/Events";
-import RenderizadoCondicional from "./Components/RenderizadoCondicional";
-import Refs from "./Components/Refs";
-import Context from "./Components/Context";
-import ListOfPostsWithDataFetched from "./Components/HOC";
-import Hooks from "./Components/Hooks";
-import PaintingList from "./Components/PaintingLists";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import Routes from "./Routes";
 
 function App() {
     return (
-        <>
-            <h1>Props</h1>
-            <Props />
-            <h1>State</h1>
-            <State />
-            <h1>ToDo</h1>
-            <Events />
-            <h1>Renderizado condicional</h1>
-            <RenderizadoCondicional />
-            <h1>Refs</h1>
-            <Refs />
-            <h1>Context</h1>
-            <Context />
-            <h1>HOC</h1>
-            <ListOfPostsWithDataFetched url="https://jsonplaceholder.typicode.com/posts" />
-            <h1>Hooks</h1>
-            <Hooks />
-            <h1>Painting lists</h1>
-            <PaintingList />
-        </>
+        <Router>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/props">Props</Link>
+                    </li>
+                    <li>
+                        <Link to="/state">State</Link>
+                    </li>
+                    <li>
+                        <Link to="/events">Events</Link>
+                    </li>
+                    <li>
+                        <Link to="/renderizado-condicional">Renderizado Condicional</Link>
+                    </li>
+                    <li>
+                        <Link to="/refs">Refs</Link>
+                    </li>
+                    <li>
+                        <Link to="/context">Context</Link>
+                    </li>
+                    <li>
+                        <Link to="/hoc">HOC</Link>
+                    </li>
+                    <li>
+                        <Link to="/hooks">Hooks</Link>
+                    </li>
+                    <li>
+                        <Link to="/painting-list">Painting List</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes />
+        </Router>
     );
 }
 
---
+++ React/src/Routes/index.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import Props from "../Components/Props";
+import State from "../Components/State";
+import Events from "../Components/Events";
+import RenderizadoCondicional from "../Components/RenderizadoCondicional";
+import Refs from "../Components/Refs";
+import Context from "../Components/Context";
+import ListOfPostsWithDataFetched from "../Components/HOC";
+import Hooks from "../Components/Hooks";
+import PaintingList from "../Components/PaintingLists";
+
+function RoutesPage() {
+    return (
+        <Routes>
+            <Route path="/props" element={<Props />} />
+            <Route path="/state" element={<State />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/renderizado-condicional" element={<RenderizadoCondicional />} />
+            <Route path="/refs" element={<Refs />} />
+            <Route path="/context" element={<Context />} />
+            <Route
+                path="/hoc"
+                element={<ListOfPostsWithDataFetched url="https://jsonplaceholder.typicode.com/posts" />}
+            />
+            <Route path="/hooks" element={<Hooks />} />
+            <Route path="/painting-list" element={<PaintingList />} />
+        </Routes>
+    );
+}
+export default RoutesPage;

0c0cfc685614fc65580b1af37cfe25b276258486
NicolasFontana <nico.fontana12@gmail.com>
2023-06-20 14:21:47 -0300

    Added functionality to order moves

--- React/src/Components/TicTacToe/Components/Board/index.tsx
+++ React/src/Components/TicTacToe/Components/Board/index.tsx
@@ -32,24 +32,26 @@ function Board({
         status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
     }
 
+    const boardRows = [];
+    for (let row = 0; row < 3; row++) {
+        const boardColumns = [];
+        for (let col = 0; col < 3; col++) {
+            const squareIndex = row * 3 + col;
+            boardColumns.push(
+                <Square key={squareIndex} value={squares[squareIndex]} onSquareClick={() => handleClick(squareIndex)} />
+            );
+        }
+        boardRows.push(
+            <div key={row} className={styles.boardRow}>
+                {boardColumns}
+            </div>
+        );
+    }
+
     return (
         <>
-            <div className="status">{status}</div>
-            <div className={styles.boardRow}>
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className={styles.boardRow}>
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className={styles.boardRow}>
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            <div className={styles.status}>{status}</div>
+            {boardRows}
         </>
     );
 }
--- React/src/Components/TicTacToe/Components/Game/index.tsx
+++ React/src/Components/TicTacToe/Components/Game/index.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Board from "../Board";
+import styles from "./index.module.scss";
 
 export type Fichas = "X" | "O";
 
 function Game() {
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
+    const [isAscending, setIsAscending] = useState(true);
     const currentSquares = history[currentMove];
     const xIsNext = currentMove % 2 === 0;
 
@@ -19,29 +21,40 @@ function Game() {
         setCurrentMove(nextMove);
     };
 
-    const moves = history.map((_, move) => {
+    let moves = history.map((_, move) => {
         let description;
         if (move > 0) {
             description = "Ir al movimiento #" + move;
         } else {
             description = "Ir al inicio del juego";
         }
-        return move !== history.length - 1 ? (
-            <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
-            </li>
-        ) : (
+        const isCurrentMove = move === history.length - 1;
+        return isCurrentMove ? (
             <li key={move}>
                 <p>Estás en el movimiento #{move}</p>
             </li>
+        ) : (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
         );
     });
+
+    const toggleOrder = () => {
+        setIsAscending(!isAscending);
+    };
+
+    if (!isAscending) {
+        moves = moves.reverse();
+    }
+
     return (
-        <div className="game">
-            <div className="game-board">
+        <div className={styles.game}>
+            <div className={styles.gameBoard}>
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
-            <div className="game-info">
+            <div className={styles.gameInfo}>
+                <button onClick={toggleOrder}>Cambiar orden: {isAscending ? "Ascendente" : "Descendente"}</button>
                 <ol>{moves}</ol>
             </div>
         </div>

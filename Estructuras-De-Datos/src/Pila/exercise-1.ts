97fbc6a96898c5db02ce83c1a463f9f6cad6d24d
NicolasFontana <nico.fontana12@gmail.com>
2023-06-30 10:34:08 -0300

    Finished exercise 1 of 'Pila'

---
+++ Estructuras-De-Datos/src/Pila/exercise-1.ts
@@ -0,0 +1,21 @@
+class Pila<T> {
+    private items: T[];
+    constructor() {
+        this.items = [];
+    }
+    push(item: T): void {
+        this.items.push(item);
+    }
+    pop(): T | undefined {
+        return this.items.pop();
+    }
+    size(): number {
+        return this.items.length;
+    }
+}
+
+const pila = new Pila();
+pila.push(1);
+pila.push(2);
+pila.pop();
+console.log(pila.size());
